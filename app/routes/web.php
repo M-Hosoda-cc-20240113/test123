17dd9c50c1ea3a796cf6113c642eb00083ab8c5e
miyawaki <n.miyawaki.carecon@gmail.com>
2021-04-02 18:01:03 +0900

    first create auth

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\TestController;
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', [TestController::class, 'index'])->name('test.index');
+// Route::get('/test', [TestController::class, 'index'])->name('test.index');
 
-// Route::get('/test', function () {
-//     return view('test');
-// });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
