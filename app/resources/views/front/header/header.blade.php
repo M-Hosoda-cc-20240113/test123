f5ea1b8843c0808bb199d66f0e54b609e794c527
hiroki <nakamurahiroki@hiroki.local>
2021-04-05 18:29:14 +0900

    update: style

---
+++ app/resources/views/front/header/header.blade.php
@@ -0,0 +1,7 @@
+<header class="Header">
+    <div class="Header__loginWrap">
+        <a href="" class="Header__textLink mr-a">Aegis</a> 
+        @include('atoms.RegularBtn', ['text' => '新規登録はこちら', 'class' => 'w-15 h-40px'])
+        <a href="" class="Header__textLink">ログイン</a> 
+    </div>
+</header>
