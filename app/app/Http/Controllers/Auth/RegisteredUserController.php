36dc7be9e90d59930e148a1d92fbb3a7ea0252e0
miyawaki <n.miyawaki.carecon@gmail.com>
2021-04-05 18:25:38 +0900

    仮コミット#1

--- app/app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,15 +33,28 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|confirmed|min:8',
+            // 'name' => 'required|string|max:255',
+            'sei'       => 'required|string|max:255',
+            'sei_kana'  => 'required|string|max:255',
+            'mei'       => 'required|string|max:255',
+            'mei_kana'  => 'required|string|max:255',
+            'email'     => 'required|string|email|max:255|unique:users',
+            'birthday'  => 'required|string|max:255',
+            'tel'       => 'required | numeric | digits_between:8,11|unique:users',
+            'password'  => 'required|string|confirmed|min:8',
         ]);
 
         Auth::login($user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+            // 'name' => $request->name,
+            'sei'       => $request->sei,
+            'sei_kana'  => $request->sei_kana,
+            'mei'       => $request->mei,
+            'mei_kana'  => $request->mei_kana,
+            'tel'       => $request->tel,
+            'birthday'  => $request->birthday,
+            'email'     => $request->email,
+            'email_hash'=> Hash::make($request->email_hash),
+            'password'  => Hash::make($request->password),
         ]));
 
         event(new Registered($user));
