c2b69ee701e6b9bc1f2c84d544006cd0fbfad94f
hiroki <nakamurahiroki@hiroki.local>
2021-04-06 11:49:12 +0900

    update: style of welcome

---
+++ app/resources/views/atoms/Tag.blade.php
@@ -0,0 +1,8 @@
+@php
+/**
+ * @var string $component コンポーネント
+ * @var string $text テキスト
+ * @var string $class スタイルクラス 
+ */
+@endphp
+<span class="Tag {{$class}}">{{ $text }}</span>
--- app/resources/views/front/header/header.blade.php
+++ app/resources/views/front/header/header.blade.php
@@ -2,6 +2,6 @@
     <div class="Header__loginWrap">
         <a href="" class="Header__textLink mr-a">Aegis</a> 
         @include('atoms.RegularBtn', ['text' => '新規登録はこちら', 'class' => 'w-15 h-40px'])
-        <a href="" class="Header__textLink">ログイン</a> 
+        <a href="" class="Header__textLink ml-15">ログイン</a> 
     </div>
 </header>
