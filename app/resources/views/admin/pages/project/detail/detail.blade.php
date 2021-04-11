62ce001468b3e3d8cb86637831817deec93f15bb
hiroki <nakamurahiroki@hiroki.local>
2021-04-11 11:37:11 +0900

    update: style

--- app/resources/views/admin/pages/project/detail/detail.blade.php
+++ app/resources/views/admin/pages/project/detail/detail.blade.php
@@ -5,10 +5,10 @@
 @section('body')
     @include('admin.header.header')
     <div class="Container mt-30 pb-100">
-        <div class="admin-project-detail bg-light pd-20">
+        <div class="admin-project-detail">
             <p class="fw-bold fs-2">大手求人広告のUXプランナー/UXディレクター</p>
             <hr class="Horizontal">
-            @include('organisms.ProjectDetailTable')
+            @include('admin.pages.project.detail._ProjectDetailTable')
         </div>
         @include('atoms.RegularBtn', ['text' => '編集', 'class' => 'w-30 m0a'])
     </div>
