b0993e596393bb26c83388a4fa79e5955f9f6559
hiroki <nakamurahiroki@hiroki.local>
2021-04-12 15:24:58 +0900

    update: js handle logic

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,7 +1,9 @@
+const url = location.pathname;
+
 // admin
 import { project_list } from "./admin/pages/project_list";
-project_list();
+if (url === '/admin/projects') project_list();
 
 // front
 import { index } from "./front/pages/index";
-index();
+if (url === '/') index();
