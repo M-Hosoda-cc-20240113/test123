57fc9b530d6c6c35e90c76d39b304e6d6b6086a3
hiroki <nakamurahiroki@hiroki.local>
2021-04-12 14:37:09 +0900

    add: register veiw

--- app/resources/views/front/pages/register/_step1.blade.php
+++ app/resources/views/front/pages/register/_step1.blade.php
@@ -1,14 +1,22 @@
-<div class="front-register-ItemWrap mt-20">
+<div class="front-register-ItemWrap mt-50">
   <div class="front-register-ItemLeft">
     <span class="mr-10 align-self-center">氏名</span>
     @include('atoms.RegularLabel', ['text' => '必須'])
   </div>
-  <div class="d-flex w-70 m0a">
+  <div class="front-register-ItemRight">
     @include('atoms.RegularInput', [
-      'text' => '姓', 'class' => 'd-flex f-end', 'class2' => 'align-self-center mr-10', 'placeholder' => '山田'
+      'text' => '姓', 
+      'class' => 'd-flex f-end w-50', 
+      'class2' => 'align-self-center mr-10', 
+      'placeholder' => '山田',
+      'name' => 'sei'
       ])
     @include('atoms.RegularInput', [
-      'text' => '名', 'class' => 'd-flex f-end', 'class2' => 'align-self-center mr-10', 'placeholder' => '太郎'
+      'text' => '名', 
+      'class' => 'd-flex f-end w-50', 
+      'class2' => 'align-self-center mr-10', 
+      'placeholder' => '太郎',
+      'name' => 'mei'
       ])
   </div>
 </div>
@@ -18,12 +26,20 @@
     <span class="mr-10 align-self-center">フリガナ</span>
     @include('atoms.RegularLabel', ['text' => '必須'])
   </div>
-  <div class="d-flex w-70 m0a">
+  <div class="front-register-ItemRight">
     @include('atoms.RegularInput', [
-      'text' => 'セイ', 'class' => 'd-flex f-end', 'class2' => 'align-self-center mr-10', 'placeholder' => 'ヤマダ'
+      'text' => 'セイ', 
+      'class' => 'd-flex f-end w-50', 
+      'class2' => 'align-self-center mr-10', 
+      'placeholder' => 'ヤマダ',
+      'name' => 'sei_kana'
       ])
     @include('atoms.RegularInput', [
-      'text' => 'メイ', 'class' => 'd-flex f-end', 'class2' => 'align-self-center mr-10', 'placeholder' => 'タロウ'
+      'text' => 'メイ', 
+      'class' => 'd-flex f-end w-50',
+      'class2' => 'align-self-center mr-10', 
+      'placeholder' => 'タロウ',
+      'name' => 'mei_kana'
       ])
   </div>
 </div>
@@ -33,9 +49,12 @@
     <span class="mr-10 align-self-center">生年月日</span>
     @include('atoms.RegularLabel', ['text' => '必須'])
   </div>
-  <div class="d-flex w-70 m0a">
+  <div class="front-register-ItemRight">
     @include('atoms.RegularInput', [
-      'class' => 'd-flex f-start', 'class2' => 'align-self-center mr-10', 'placeholder' => '1992 / 07 / 08'
+      'class' => 'd-flex f-start', 
+      'class2' => 'align-self-center mr-10', 
+      'placeholder' => '1992 / 07 / 08',
+      'name' => 'birthday'
       ])
   </div>
 </div>
@@ -45,9 +64,12 @@
     <span class="mr-10 align-self-center">メールアドレス</span>
     @include('atoms.RegularLabel', ['text' => '必須'])
   </div>
-  <div class="d-flex w-70 m0a">
+  <div class="front-register-ItemRight">
     @include('atoms.RegularInput', [
-      'class' => 'd-flex f-start', 'class2' => 'align-self-center mr-10', 'placeholder' => 'mail@example.com'
+      'class' => 'd-flex f-start', 
+      'class2' => 'align-self-center mr-10', 
+      'placeholder' => 'mail@example.com',
+      'name' => 'email'
       ])
   </div>
 </div>
@@ -57,9 +79,28 @@
     <span class="mr-10 align-self-center">電話番号</span>
     @include('atoms.RegularLabel', ['text' => '必須'])
   </div>
-  <div class="d-flex w-70 m0a">
+  <div class="front-register-ItemRight">
     @include('atoms.RegularInput', [
-      'class' => 'd-flex f-start', 'class2' => 'align-self-center mr-10', 'placeholder' => '09012345678（ハイフン不要）'
+      'class' => 'd-flex f-start', 
+      'class2' => 'align-self-center mr-10', 
+      'placeholder' => '09012345678（ハイフン不要）',
+      'name' => 'tel'
       ])
   </div>
 </div>
+
+<div class="front-register-ItemWrap mt-20 pb-20">
+  <div class="front-register-ItemLeft">
+    <span class="mr-10 align-self-center">パスワード</span>
+    @include('atoms.RegularLabel', ['text' => '必須'])
+  </div>
+  <div class="front-register-ItemRight">
+    @include('atoms.RegularInput', [
+      'class' => 'd-flex f-start', 'class2' => 'align-self-center mr-10',
+      'type' => 'password',
+      'name' => 'password'
+      ])
+  </div>
+</div>
+
+@include('atoms.RegularBtn', ['text' => '登録', 'class' => 'w-20 m0a'])
