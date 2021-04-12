683ce18af7d7ccf7cb490d2778c17958a0bae113
hiroki <nakamurahiroki@hiroki.local>
2021-04-12 16:37:41 +0900

    update: style

--- app/resources/views/front/pages/top/top.blade.php
+++ app/resources/views/front/pages/top/top.blade.php
@@ -11,17 +11,17 @@
         @include('front.pages.top._ContentTab')
         <div class="welcome-Checkbox tab_content-js tab_active-js">
         @foreach ($response->getSkills() as $key => $skill)
-            @include('atoms.Checkbox', ['class' => 'mr-30', 'index' => $key, 'text' => $skill->name])
+            @include('atoms.Checkbox', ['class' => 'mr-10 mb-10 w-100px', 'index' => $key, 'text' => $skill->name])
         @endforeach
         </div>
         <div class="welcome-Checkbox tab_content-js">
         @foreach ($response->getPositions() as $key => $position)
-            @include('atoms.Checkbox', ['class' => 'mr-30', 'index' => $key, 'text' => $position->name])
+            @include('atoms.Checkbox', ['class' => 'mr-10 mb-10 w-200px', 'index' => $key, 'text' => $position->name])
         @endforeach
         </div>
         <div class="welcome-Checkbox tab_content-js">
         @foreach ($response->getStations() as $key => $station)
-            @include('atoms.Checkbox', ['class' => 'mr-30', 'index' => $key, 'text' => $station->name])
+            @include('atoms.Checkbox', ['class' => 'mr-10 mb-10 w-100px', 'index' => $key, 'text' => $station->name])
         @endforeach
         </div>
         <div class="welcome-ProjectCard mt-30">
