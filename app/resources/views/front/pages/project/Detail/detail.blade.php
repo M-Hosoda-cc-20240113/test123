187c70a511f95613c18145c3263bedaf108c6419
hiroki <hiroki.nakamura78@gmail.com>
2021-04-15 16:07:04 +0900

    update: style

--- app/resources/views/front/pages/project/Detail/detail.blade.php
+++ app/resources/views/front/pages/project/Detail/detail.blade.php
@@ -10,6 +10,7 @@
             @include('front.pages.project.detail._ProjectDetailTable', ['response' => $response])
         </div>
         @include('atoms.RegularBtn', ['link' => '/' ,'text' => '応募する', 'class' => 'w-30 m0a'])
+        <p class="mt-20 ta-center fs-1.2">※クリックで応募が完了します。担当営業が2営業日以内に電話で連絡いたします。</p>
     </div>
 @include('front.footer.footer')
 @endsection
