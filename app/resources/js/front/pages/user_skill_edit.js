1d06d61e8fec3f7d008acfde94c9b9fca974e3bf
hiroki <hiroki.nakamura78@gmail.com>
2021-04-22 18:17:15 +0900

    update: logic js

--- app/resources/js/front/pages/user_skill_edit.js
+++ app/resources/js/front/pages/user_skill_edit.js
@@ -1,6 +1,7 @@
 export function user_skill_edit() {
   // 要素
-  const rows = document.querySelectorAll('.p-formGroupUnit--2col__itemForSkill');
+  const rows = document.querySelectorAll(
+      '.p-formGroupUnit--2col__itemForSkill');
   const add_button = document.querySelector('.js-add');
   const template = document.querySelector('template');
   const REGISTER_LIMIT = 10;
@@ -15,35 +16,32 @@ export function user_skill_edit() {
     });
   });
 
-  try {
-    add_button.addEventListener('click', (e) => {
-      // 要素
-      const fragment = document.createDocumentFragment();
-      const clone = document.importNode(template.content, true);
-      const parent = document.querySelector('.js-form');
+  add_button.addEventListener('click', (e) => {
+    // 要素
+    const fragment = document.createDocumentFragment();
+    const clone = document.importNode(template.content, true);
+    const parent = document.querySelector('.js-form');
 
-      // 10項目以上登録できないようにする
-      if (parent.querySelectorAll(
-          '.p-formGroupUnit--2col__itemForSkill').length >= REGISTER_LIMIT) {
-        throw new Error('10項目以上登録できません。');
+    // 10項目以上登録できないようにする
+    if (parent.querySelectorAll('.p-formGroupUnit--2col__itemForSkill').length >= REGISTER_LIMIT) {
+      alert('10項目以上登録できません');
+      return;
+    }
+
+    fragment.appendChild(clone);
+
+    parent.insertBefore(fragment, add_button);
+
+    // 追加要素に対してイベントの設置
+    const target = parent.querySelectorAll(
+        '.p-formGroupUnit--2col__itemForSkill')[parent.querySelectorAll(
+        '.p-formGroupUnit--2col__itemForSkill').length - 1];
+
+    target.addEventListener('click', (e) => {
+      if (e.offsetX > target.offsetWidth) {
+        e.target.parentNode.removeChild(e.target);
       }
-
-      fragment.appendChild(clone);
-
-      parent.insertBefore(fragment, add_button);
-
-      // 追加要素に対してイベントの設置
-      const target = parent.querySelectorAll(
-          '.p-formGroupUnit--2col__itemForSkill')[parent.querySelectorAll(
-          '.p-formGroupUnit--2col__itemForSkill').length - 1];
-
-      target.addEventListener('click', (e) => {
-        if (e.offsetX > target.offsetWidth) {
-          e.target.parentNode.removeChild(e.target);
-        }
-      });
     });
-  } catch (e) {
-    console.log(e);
-  }
+  });
+
 }
