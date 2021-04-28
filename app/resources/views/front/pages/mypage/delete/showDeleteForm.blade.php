393a40ee632b629da4625be5060b0cc304d0c06b
MiyawakiNoriyuki <n.miyawaki.carecon@gmail.com>
2021-04-28 16:15:43 +0900

    fix:front mypage

--- app/resources/views/front/pages/mypage/delete/showDeleteForm.blade.php
+++ app/resources/views/front/pages/mypage/delete/showDeleteForm.blade.php
@@ -6,6 +6,7 @@
 @extends('front.layout')
 @section('body')
     @component('front.header.header', ['href' => route('front.index')])
+        @include('front.header._link_mypage')
         @include('front.header._link_login_logout')
     @endcomponent
     <div class="l-container">
