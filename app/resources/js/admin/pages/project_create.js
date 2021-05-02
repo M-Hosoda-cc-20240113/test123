11e3ad1c0476dde3d141160280872b321a7824ac
hiroki <hiroki.nakamura78@gmail.com>
2021-05-02 15:35:36 +0900

    add: comment

--- app/resources/js/admin/pages/project_create.js
+++ app/resources/js/admin/pages/project_create.js
@@ -1,6 +1,9 @@
 import {CopyFragment} from '../../front/CopyFragment';
 
 export function project_create() {
+  // ポジション選択
   new CopyFragment(0, 10, true);
+
+  // スキル選択
   new CopyFragment(1, 10, true);
 }
