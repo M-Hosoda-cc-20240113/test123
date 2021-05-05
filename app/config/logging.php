a5d4411af728154f7560d8ca79f5321fbde6fa2d
hiroki <hiroki.nakamura78@gmail.com>
2021-05-05 13:37:01 +0900

    feat: logging setting

--- app/config/logging.php
+++ app/config/logging.php
@@ -100,6 +100,13 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+        'stdout' => [
+            'driver' => 'monolog',
+            'handler' => StreamHandler::class,
+            'with' => [
+                'stream' => 'php://stdout',
+            ],
+            'level' => 'debug',
+        ],
     ],
-
 ];
