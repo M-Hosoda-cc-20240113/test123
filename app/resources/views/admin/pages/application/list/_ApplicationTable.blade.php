722df31c313098cc531b0a626bc29550f29555ec
hiroki <hiroki.nakamura78@gmail.com>
2021-05-05 17:31:38 +0900

    css: admin pages

--- app/resources/views/admin/pages/application/list/_ApplicationTable.blade.php
+++ app/resources/views/admin/pages/application/list/_ApplicationTable.blade.php
@@ -1,37 +1,37 @@
-<table class="RegularTable">
-    <thead class="RegularTable__head">
-    <tr class="RegularTable__headRow">
-        <th class="RegularTable__headRowItem">案件名</th>
-        <th class="RegularTable__headRowItem">ユーザー名</th>
-        <th class="RegularTable__headRowItem">ステータス</th>
-        <th class="RegularTable__headRowItem">応募日時</th>
-        <th class="RegularTable__headRowItem">面談予定日</th>
-        <th class="RegularTable__headRowItem">アサイン</th>
+<table class="p-table">
+    <thead class="p-table__head">
+    <tr class="p-table__headRow">
+        <th class="p-table__headRowItem">案件名</th>
+        <th class="p-table__headRowItem">ユーザー名</th>
+        <th class="p-table__headRowItem">ステータス</th>
+        <th class="p-table__headRowItem">応募日時</th>
+        <th class="p-table__headRowItem">面談予定日</th>
+        <th class="p-table__headRowItem">アサイン</th>
     </tr>
     </thead>
 
-    <tbody class="RegularTable__body">
+    <tbody class="p-table__body">
     @foreach ($response->getApplications()['applications'] as $key => $application)
-        <tr class="RegularTable__bodyRow" data-href="">
-            <td class="RegularTable__bodyRowItem">
+        <tr class="p-table__bodyRow" data-href="">
+            <td class="p-table__bodyRowItem">
                 <a class="d-block w-100"
                    href="{{ route('project.detail', ['project_id' => $application->projects->id] )}}">{{ $application->projects->name ?? '' }}</a>
             </td>
-            <td class="RegularTable__bodyRowItem">
+            <td class="p-table__bodyRowItem">
                 <a class="d-block w-100"
                    href="{{ route('user.detail', ['user_id' => $application->users->id] )}}">{{ $application->users->sei ?? ''  }}
                     &nbsp;{{ $application->users->mei ?? '' }}</a>
             </td>
-            <td class="RegularTable__bodyRowItem">
+            <td class="p-table__bodyRowItem">
                 {{ ViewHelper::Status( $response->getApplications()['statuses'][$key]->status ?? '' )}}<br>
             </td>
-            <td class="RegularTable__bodyRowItem">
+            <td class="p-table__bodyRowItem">
                 {{ ViewHelper::YmdReplace( $application->created_at ?? '' )}}<br>
             </td>
-            <td class="RegularTable__bodyRowItem">
+            <td class="p-table__bodyRowItem">
                 {{ ViewHelper::YmdReplace( $application->interview_date ?? '未定' )}}<br>
             </td>
-            <td class="RegularTable__bodyRowItem--textCenter">
+            <td class="p-table__bodyRowItem--textCenter">
                 <form action="{{ route('assignment.register') }}" method="post">
                     {{ @csrf_field() }}
                     <input type="hidden" name="user_id" value="{{ $application->users->id }}">
--- app/resources/views/admin/pages/dashboard/dashboard.blade.php
+++ app/resources/views/admin/pages/dashboard/dashboard.blade.php
@@ -6,8 +6,10 @@
     @component('admin.header.header', ['href' => route('front.index')])
         @include('admin.header._link_login_logout')
     @endcomponent
-    <div class="Container mt-30">
-      @include('admin.pages.dashboard._RegularList')
+    <div class="l-container--2col">
+        <div class="l-sidebar">
+            @include('admin.pages.dashboard._RegularList')
+        </div>
     </div>
 
     @include('admin.footer.footer')
