9940915a25dcd7bb43eb96073515a73cae6ec0ed
hiroki <hiroki.nakamura78@gmail.com>
2021-05-06 17:19:53 +0900

    css: update

--- app/resources/views/admin/pages/project/detail/_ProjectDetailTable.blade.php
+++ app/resources/views/admin/pages/project/detail/_ProjectDetailTable.blade.php
@@ -1,53 +1,53 @@
-<table class="ProjectDetailTable">
+<table class="p-itemDetailTable">
     <tbody>
-    <tr class="ProjectDetailTable__row">
-        <th class="ProjectDetailTable__head">単価</th>
-        <td class="ProjectDetailTable__body">{{ $response->getProject()->min_unit_price ?? ''}}
+    <tr class="p-itemDetailTable__row">
+        <th class="p-itemDetailTable__head">単価</th>
+        <td class="p-itemDetailTable__body">{{ $response->getProject()->min_unit_price ?? ''}}
             〜{{ $response->getProject()->max_unit_price ?? ''}}万
         </td>
-        <th class="ProjectDetailTable__head">最寄り駅</th>
-        <td class="ProjectDetailTable__body">{{ $response->getProject()->station->name ?? ''}}</td>
+        <th class="p-itemDetailTable__head">最寄り駅</th>
+        <td class="p-itemDetailTable__body">{{ $response->getProject()->station->name ?? ''}}</td>
     </tr>
-    <tr class="ProjectDetailTable__row">
-        <th class="ProjectDetailTable__head">案件内容</th>
-        <td class="ProjectDetailTable__body">{{ ViewHelper::nl2brWithAnchor( $response->getProject()->description ?? '' )}}</td>
-        <th class="ProjectDetailTable__head">必須</th>
-        <td class="ProjectDetailTable__body">{{ ViewHelper::nl2brWithAnchor( $response->getProject()->required_condition ?? '' )}}</td>
+    <tr class="p-itemDetailTable__row">
+        <th class="p-itemDetailTable__head">案件内容</th>
+        <td class="p-itemDetailTable__body">{{ ViewHelper::nl2brWithAnchor( $response->getProject()->description ?? '' )}}</td>
+        <th class="p-itemDetailTable__head">必須</th>
+        <td class="p-itemDetailTable__body">{{ ViewHelper::nl2brWithAnchor( $response->getProject()->required_condition ?? '' )}}</td>
     </tr>
-    <tr class="ProjectDetailTable__row">
-        <th class="ProjectDetailTable__head">就業時間</th>
-        <td class="ProjectDetailTable__body">{{ ViewHelper::timeReplace( $response->getProject()->work_start ?? '' )}}
+    <tr class="p-itemDetailTable__row">
+        <th class="p-itemDetailTable__head">就業時間</th>
+        <td class="p-itemDetailTable__body">{{ ViewHelper::timeReplace( $response->getProject()->work_start ?? '' )}}
             〜{{ ViewHelper::timeReplace( $response->getProject()->work_end ?? '' )}}</td>
-        <th class="ProjectDetailTable__head">精算幅</th>
-        <td class="ProjectDetailTable__body">{{ $response->getProject()->min_operation_time ?? ''}}
+        <th class="p-itemDetailTable__head">精算幅</th>
+        <td class="p-itemDetailTable__body">{{ $response->getProject()->min_operation_time ?? ''}}
             〜{{ $response->getProject()->max_operation_time ?? ''}}時間
         </td>
     </tr>
-    <tr class="ProjectDetailTable__row">
-        <th class="ProjectDetailTable__head">職種</th>
-        <td class="ProjectDetailTable__body">
+    <tr class="p-itemDetailTable__row">
+        <th class="p-itemDetailTable__head">職種</th>
+        <td class="p-itemDetailTable__body">
             @foreach ($response->getProject()->positions as $position)
                 <span class="c-label u-mt-5 u-mr-10">{{ $position->name ?? '' }}</span>
             @endforeach
         </td>
-        <th class="ProjectDetailTable__head">スキル</th>
-        <td class="ProjectDetailTable__body">
+        <th class="p-itemDetailTable__head">スキル</th>
+        <td class="p-itemDetailTable__body">
             @foreach ($response->getProject()->skills as $skill)
                 <span class="c-label u-mt-5 u-mr-10">{{ $skill->name ?? '' }}</span>
             @endforeach
         </td>
     </tr>
-    <tr class="ProjectDetailTable__row">
-        <th class="ProjectDetailTable__head">尚可</th>
-        <td class="ProjectDetailTable__body">{{ ViewHelper::nl2brWithAnchor( $response->getProject()->better_condition ?? '' )}}</td>
-        <th class="ProjectDetailTable__head">特徴</th>
-        <td class="ProjectDetailTable__body">{{ ViewHelper::nl2brWithAnchor( $response->getProject()->feature ?? '' )}}</td>
+    <tr class="p-itemDetailTable__row">
+        <th class="p-itemDetailTable__head">尚可</th>
+        <td class="p-itemDetailTable__body">{{ ViewHelper::nl2brWithAnchor( $response->getProject()->better_condition ?? '' )}}</td>
+        <th class="p-itemDetailTable__head">特徴</th>
+        <td class="p-itemDetailTable__body">{{ ViewHelper::nl2brWithAnchor( $response->getProject()->feature ?? '' )}}</td>
     </tr>
-    <tr class="ProjectDetailTable__row">
-        <th class="ProjectDetailTable__head">出勤回数</th>
-        <td class="ProjectDetailTable__body">週 {{ $response->getProject()->weekly_attendance ?? ''}} 回</td>
-        <th class="ProjectDetailTable__head">案件元会社</th>
-        <td class="ProjectDetailTable__body">{{ $response->getProject()->agent->name ?? ''}}</td>
+    <tr class="p-itemDetailTable__row">
+        <th class="p-itemDetailTable__head">出勤回数</th>
+        <td class="p-itemDetailTable__body">週 {{ $response->getProject()->weekly_attendance ?? ''}} 回</td>
+        <th class="p-itemDetailTable__head">案件元会社</th>
+        <td class="p-itemDetailTable__body">{{ $response->getProject()->agent->name ?? ''}}</td>
     </tr>
     </tbody>
 </table>
--- app/resources/views/admin/pages/project/detail/detail.blade.php
+++ app/resources/views/admin/pages/project/detail/detail.blade.php
@@ -1,3 +1,8 @@
+@php
+/**
+ * @var \App\Services\AdminProject\ProjectDetail\ProjectDetailResponse $response
+ */
+@endphp
 @extends('admin.layout')
 
 @section('title', '案件詳細|Aegis')
@@ -6,34 +11,30 @@
     @component('admin.header.header', ['href' => route('front.index')])
         @include('admin.header._link_login_logout')
     @endcomponent
-    <div class="Container mt-30 pb-100">
-        @include('admin.bread_crumb._BreadCrumb')
-        <div class="admin-project-detail">
-            <p class="fw-bold fs-20">{{ $response->getProject()->name ?? '' }}
-                {{ $response->getProject()->decided ? '※応募終了' : '' }}
-            </p>
-            <hr class="Horizontal">
-            @include('admin.pages.project.detail._ProjectDetailTable', ['response' => $response])
+    <div class="l-container">
+        <div class="l-main">
+            <div class="p-mainItem">
+                <h2 class="p-level2Heading">{{ $response->getProject()->name ?? '' }}{{ $response->getProject()->decided ? '※応募終了' : '' }}</h2>
+                @include('admin.pages.project.detail._ProjectDetailTable', ['response' => $response])
+
+                <a class="c-button u-db u-m0a u-w-30-pc u-mt-30" href="{{ route('project.edit.form', ['project_id' => $response->getProject()->id]) }}">編集</a>
+                {{--   編集  --}}
+
+                <form action="{{ route('project.toggle') }}" method="POST">
+                    {{ @csrf_field() }}
+                    <input type="hidden" name="project_id" value="{{ $response->getProject()->id }}">
+                    <button type="submit" class="c-button u-db u-m0a u-w-30-pc u-mt-10">{{ $response->getProject()->decided ? '応募開始' : '応募終了' }}</button>
+                </form>
+                {{--   応募終了  --}}
+
+                <form action="{{ route('project.delete') }}" method="POST">
+                    {{ @csrf_field() }}
+                    <input type="hidden" name="project_id" value="{{ $response->getProject()->id }}">
+                    <button type="submit" class="c-button--warning u-db u-m0a u-w-30-pc u-mt-10" id="alert_button">削除</button>
+                </form>
+                {{--   削除  --}}
+            </div>
         </div>
-        @if($errors->all())
-            @foreach($errors->all() as $error)
-                <p class="c-text--warning" style="color: red">{{ $error }}</p>
-            @endforeach
-        @endif
-        @include('atoms.RegularBtn', ['text' => '編集', 'class' => 'w-30 ', 'link' => route('project.edit.form', ['project_id' => $response->getProject()->id])])
-        <form action="{{ route('project.toggle') }}" method="POST">
-            {{ @csrf_field() }}
-            <input type="hidden" name="project_id" value="{{ $response->getProject()->id }}">
-            <button type="submit"
-                    class="RegularBtn w-30 ">{{ $response->getProject()->decided ? '応募開始' : '応募終了' }}</button>
-        </form>
-        <form action="{{ route('project.delete') }}" method="POST">
-            {{ @csrf_field() }}
-            <input type="hidden" name="project_id" value="{{ $response->getProject()->id }}">
-            <button style="background-color: coral" type="submit" class="RegularBtn w-30 "
-                    id="alert_button">削除
-            </button>
-        </form>
     </div>
     @include('admin.footer.footer')
 @endsection
