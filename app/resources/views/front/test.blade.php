c288b534d4b182e870a8e3c39808302cf8a3eb82
hiroki <hiroki.nakamura78@gmail.com>
2021-05-06 10:19:31 +0900

    feat: dashboard

--- app/resources/views/front/test.blade.php
+++ app/resources/views/front/test.blade.php
@@ -22,31 +22,5 @@
         @endcomponent
     </div>
     <div class="l-container">
-        <nav>
-            <ul class="p-paginator">
-                <li class="p-paginator__item p-paginator__item--prev"><a class="p-paginator__link" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="8" height="12" viewbox="0 0 8 12">
-                            <g fill="none" fill-rule="evenodd">
-                                <path fill="#33313C" d="M7.41 1.41L6 0 0 6l6 6 1.41-1.41L2.83 6z"></path>
-                            </g>
-                        </svg>
-                    </a></li>
-                <li class="p-paginator__item"><a class="p-paginator__link" href="#">...</a></li>
-                <li class="p-paginator__item"><a class="p-paginator__link" href="#">1</a></li>
-                <li class="p-paginator__item active"><a class="p-paginator__link" href="#">2</a></li>
-                <li class="p-paginator__item"><a class="p-paginator__link" href="#">3</a></li>
-                <li class="p-paginator__item"><a class="p-paginator__link" href="#">4</a></li>
-                <li class="p-paginator__item"><a class="p-paginator__link" href="#">5</a></li>
-                <li class="p-paginator__item"><a class="p-paginator__link" href="#">6</a></li>
-                <li class="p-paginator__item"><a class="p-paginator__link" href="#">...</a></li>
-                <li class="p-paginator__item p-paginator__item--next"><a class="p-paginator__link" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="8" height="12" viewbox="0 0 8 12">
-                            <g fill="none" fill-rule="evenodd">
-                                <path fill="#33313C" d="M7.41 1.41L6 0 0 6l6 6 1.41-1.41L2.83 6z"></path>
-                            </g>
-                        </svg>
-                    </a></li>
-            </ul>
-        </nav>
     </div>
 @endsection
