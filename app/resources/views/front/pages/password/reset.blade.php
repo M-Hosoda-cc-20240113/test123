ca12ba5c5e900aca77cd02737fe825d60907b0c2
hiroki <hiroki.nakamura78@gmail.com>
2021-05-10 09:06:27 +0900

    css: update

--- app/resources/views/front/pages/password/reset.blade.php
+++ app/resources/views/front/pages/password/reset.blade.php
@@ -7,19 +7,19 @@
     @include('front.header._link_mypage')
     @include('front.header._link_login_logout')
   @endcomponent
-  <div class="Container mt-30">
+  <div class="l-container">
     <form action="{{ route('password.request') }}" method="post">
       {{ csrf_field() }}
       <input type="hidden" name="token" value="{{ $token }}">
       <input type="hidden" name="token2" value="{{ $token2 }}">
-      <p>パスワードの再設定を行います。新しいパスワードを入力してください。</p>
-      <p>半角8文字以上、英字大文字、英字小文字、数字を全て含むパスワードを入力してください。</p>
+      <p class="c-text">パスワードの再設定を行います。新しいパスワードを入力してください。</p>
+      <p class="c-text">半角8文字以上、英字大文字、英字小文字、数字を全て含むパスワードを入力してください。</p>
       @foreach($errors->all() as $error)
         <p>{{ $error }}</p>
       @endforeach
-      @include('atoms.Input', ['type' => 'password', 'name' => 'password', 'placeholder' => 'パスワード入力欄'])
-      @include('atoms.Input', ['type' => 'password', 'name' => 'password_confirmation', 'placeholder' => 'パスワード入力欄(確認)'])
-      <button type="submit" class="RegularBtn">再設定する</button>
+      <input class="c-input--light u-mt-20" type="password" name="password" placeholder="パスワード入力欄">
+      <input class="c-input--light u-mt-10" type="password" name="password_confirmation" placeholder="パスワード入力欄(確認)">
+      <button type="submit" class="c-button u-mt-20">再設定する</button>
     </form>
   </div>
 
--- app/resources/views/front/pages/password/reset_complete.blade.php
+++ app/resources/views/front/pages/password/reset_complete.blade.php
@@ -7,9 +7,12 @@
     @include('front.header._link_mypage')
     @include('front.header._link_login_logout')
   @endcomponent
-  <div class="Container mt-30">
-    <p>パスワードの再設定が完了しました。<br>新しいパスワードでログインしてください。</p>
-    @include('atoms.RegularBtn', ['link' => route('login'), 'text' => 'ログインページへ', 'class' => 'w-30'])
+  <div class="l-container">
+      <div class="l-main">
+          <p>パスワードの再設定が完了しました。<br class="u-dn-pc">新しいパスワードでログインしてください。</p>
+          @include('atoms.RegularBtn', ['link' => route('login'), 'text' => 'ログインページへ', 'class' => 'w-30'])
+          <a href="" class="c-button u-mt-20 u-db u-m0a u-w-30-pc">ログインページへ</a>
+      </div>
   </div>
 
   @include('front.footer.footer')
