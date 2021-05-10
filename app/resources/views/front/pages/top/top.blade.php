419b69b77d85da92eb83c4af5cb89cce466a5a3e
hiroki <hiroki.nakamura78@gmail.com>
2021-05-10 16:17:48 +0900

    css: update

--- app/resources/views/front/pages/top/top.blade.php
+++ app/resources/views/front/pages/top/top.blade.php
@@ -87,41 +87,7 @@
 
         <div class="p-mainItem u-mt-30">
             <h2 class="p-level2Heading">よくあるご質問</h2>
-            <div class="p-qa">
-                <p class="p-qa__question">
-                    <span class="p-qa__questionIcon">Q</span>
-                    利用料は無料ですか？
-                </p>
-                <div class="p-qa__button--open"></div>
-                <div class="p-qa__answer">
-                    <p>無料です。案件紹介、サポートも無料で行っております。</p>
-                </div>
-            </div>
-            <div class="p-qa">
-                <p class="p-qa__question">
-                    <span class="p-qa__questionIcon">Q</span>
-                    Aegisポイントとは何ですか？
-                </p>
-                <div class="p-qa__button--open"></div>
-                <div class="p-qa__answer">
-                    <p>
-                        当サービス内で付与されるポイントです。毎月一日に5000ポイントが付与されます。<br>
-                        Aegis経由で案件をお受けいただいた場合、1ポイント1円で振り込みいたします。
-                    </p>
-                </div>
-            </div>
-            <div class="p-qa">
-                <p class="p-qa__question">
-                    <span class="p-qa__questionIcon">Q</span>
-                    全ての案件が掲載されていますか？
-                </p>
-                <div class="p-qa__button--open"></div>
-                <div class="p-qa__answer">
-                    <p>
-                        Aegis上で公開できない案件も多数ございます。お気軽にお問い合わせください。
-                    </p>
-                </div>
-            </div>
+            @include('front.pages.top._FAQ')
         </div>
     </div>
 
