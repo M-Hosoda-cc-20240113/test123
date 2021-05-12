2b7211cf996efbb08954e941f0d155af9f4289d0
hiroki <hiroki.nakamura78@gmail.com>
2021-05-12 11:50:25 +0900

    add: new line

--- app/resources/js/common.js
+++ app/resources/js/common.js
@@ -1,6 +1,8 @@
 import {Drawer} from './front/Drawer';
 
 export function common() {
+
+  // ドロワー
   const drawer = document.querySelector('.c-drawer--gray');
   const drawer_content = document.querySelector('.p-drawerContents');
   /**
