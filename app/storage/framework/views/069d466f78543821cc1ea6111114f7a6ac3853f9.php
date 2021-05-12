4fb0e7774e988c2b1a55e1270fe4e096379bcf24
MiyawakiNoriyuki <n.miyawaki.carecon@gmail.com>
2021-05-12 16:33:05 +0900

    fix: delete views file

--- app/storage/framework/views/069d466f78543821cc1ea6111114f7a6ac3853f9.php
+++
@@ -1,8 +0,0 @@
-<ul class="p-list">
-  <li class="p-list__item--header">各種一覧</li>
-  <li class="p-list__item"><a class="RegularList__itemInner" href="<?php echo e(route('user.list')); ?>">ユーザー一覧</a></li>
-  <li class="p-list__item"><a class="RegularList__itemInner" href="<?php echo e(route('project.list')); ?>">案件一覧</a></li>
-  <li class="p-list__item"><a class="RegularList__itemInner" href="<?php echo e(route('application.list')); ?>">応募状況一覧</a></li>
-  <li class="p-list__item"><a class="RegularList__itemInner" href="<?php echo e(route('assignment.list')); ?>">稼働状況一覧</a></li>
-</ul>
-<?php /**PATH /aegis/resources/views/admin/pages/dashboard/_RegularList.blade.php ENDPATH**/ ?>
--- app/storage/framework/views/9f29d06d97c49fd63321ea24513759e51f130d37.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @var string $component コンポーネント
- * @var string $text テキスト
- * @var string $class スタイルクラス 
- */
-?>
-<span class="Tag <?php echo e($class ?? ''); ?>"><?php echo e($text ?? ''); ?></span>
-<?php /**PATH /aegis/resources/views/atoms/Tag.blade.php ENDPATH**/ ?>
