861a998e05fde78537966da39148ca1c462a000d
hiroki <nakamurahiroki@hiroki.local>
2021-05-13 13:00:17 +0900

    css: admin footer

--- app/resources/views/admin/footer/footer.blade.php
+++ app/resources/views/admin/footer/footer.blade.php
@@ -1,8 +1,10 @@
-<footer class="Footer">
-    <div class="Footer__linkWrap">
-        <a href="" class="Footer__textLink">Aegis利用規約</a> 
-        <a href="" class="Footer__textLink">運営会社</a> 
-        <a href="" class="Footer__textLink">個人情報保護方針</a> 
-    </div>
-    <p class="Footer__copy"><span>&copy;</span>2021 Carecon Inc</p>
-</footer>
+<div class="l-footer">
+    <footer class="p-footer">
+        <div class="p-footer__linkWrap">
+            <a href="" class="p-footer__textLink">Aegis利用規約</a>
+            <a href="" class="p-footer__textLink">運営会社</a>
+            <a href="" class="p-footer__textLink">個人情報保護方針</a>
+        </div>
+        <p class="p-footer__copy"><span>&copy;</span>2021 Carecon Inc</p>
+    </footer>
+</div>
