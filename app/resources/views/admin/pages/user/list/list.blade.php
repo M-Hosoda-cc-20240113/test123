ed6d887d8be064ba5458e9477442e412d59c7a9a
hiroki <nakamurahiroki@hiroki.local>
2021-05-14 16:27:25 +0900

    update: style

--- app/resources/views/admin/pages/user/list/list.blade.php
+++ app/resources/views/admin/pages/user/list/list.blade.php
@@ -25,15 +25,87 @@
 
     <div class="l-container">
         @include('admin.bread_crumb._BreadCrumb')
-        @if($errors->all())
-            <p class="c-text--bold u-mt-20">検索条件が不正です。</p>
-            @foreach($errors->all() as $error)
-                <p class="c-text--warning">{{ $error }}</p>
-            @endforeach
-        @endif
-        <ul class="p-searchTab u-mt-20">
-            <li class="p-searchTab__item u-w-60-sp js-tab is-active"><span>ユーザーカテゴリー</span></li>
-        </ul>
+        <div class="l-main">
+            <div class="p-mainItem u-mt-20">
+                @if($errors->all())
+                    <p class="c-text--bold u-mt-20">検索条件が不正です。</p>
+                    @foreach($errors->all() as $error)
+                        <p class="c-text--warning">{{ $error }}</p>
+                    @endforeach
+                @endif
+                <h2 class="p-level2Heading">ユーザー検索</h2>
+                <form class="js-form" action="{{ route('user.search') }}" method="get">
+
+                    <div class="p-formGroupUnit--2col">
+                        <div class="p-formGroup p-formGroupUnit--2col__item--left u-pr-30pc">
+                            <p class="c-text--bold">ユーザーカテゴリ</p>
+                            <label class="c-select" for="">
+                                <select name="" id="">
+                                    <option value="">選択してください</option>
+                                    <option value="">新規</option>
+                                    <option value="">既存</option>
+                                    <option value="">稼働中</option>
+                                    <option value="">待機中</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div class="p-formGroup p-formGroupUnit--2col__item u-pr-30pc">
+                            <p class="c-text--bold">ステータス(今月)</p>
+                            <label class="c-select" for="">
+                                <select name="" id="">
+                                    <option value="">選択してください</option>
+                                    <option value="">営業開始</option>
+                                    <option value="">未営業</option>
+                                    <option value="">新規稼働</option>
+                                    <option value="">既存稼働</option>
+                                    <option value="">面談</option>
+                                </select>
+                            </label>
+                        </div>
+                    </div>
+
+                    <div class="p-formGroupUnit--2col u-mt-20 u-ff-column js-parent">
+                        <div class="p-formGroupUnit--2col__title">
+                            <p class="c-text--bold">スキル</p>
+                            <p class="c-text--bold u-pl-15">経験</p>
+                        </div>
+                        <div class="p-formGroupUnit--2col__itemForSkill js-content js-remove">
+                            <label class="p-formGroupUnit--2col__label" for="">
+                                <select name="skill_ids[]" id="skill_id" class="c-input">
+                                    <option value="">選択してください</option>
+                                    @foreach($response->getSkills() as $skill)
+                                        <option
+                                                value="{{ $skill->id }}">{{ $skill->name }}</option>
+                                    @endforeach
+                                </select>
+                            </label>
+
+                            <label class="p-formGroupUnit--2col__label--after" for="">
+                                <select name="level_ids[]" id="" class="c-input">
+                                    <option value="">選択してください</option>
+                                    @foreach($response->getLevels() as $level)
+                                        <option
+                                                value="{{ $level->id }}">{{ $level->level }}</option>
+                                    @endforeach
+                                </select>
+                            </label>
+                        </div>
+                        {{--    itemForSkill      --}}
+                        <img class="c-icon--clickable u-m0a js-add" src="/images/icons/icon_add.png" alt="">
+                    </div>
+                    {{--Level Skill--}}
+
+                    <div class="p-formGroup">
+                        <span class="c-text--bold">営業開始月</span>
+                        <input class="c-input u-w-50-pc u-mt-5" name="operation_start_month" type="date" value="{{ old('operation_start_month') }}">
+                    </div>
+                    {{-- 営業開始月 --}}
+
+                    <button type="submit" class="c-button u-mt-20 u-db u-w-30-pc u-m0a">検索</button>
+                    {{--  検索ボタン（dashboard連動）  --}}
+                </form>
+            </div>
+        </div>
 
         <template id="skill_row">
             <div class="p-formGroupUnit--2col__itemForSkill js-content js-remove">
@@ -58,92 +130,6 @@
         </template>
         {{--   テンプレート    --}}
 
-        <form class="js-form" action="{{ route('user.search') }}" method="get">
-            {{--checkbox--}}
-            <div class="p-checkboxUnit js-tab_content">
-                <label class="p-checkbox p-checkboxUnit__item">新規
-                    <input value=1 name="new_user"
-                           type="checkbox" @if(request()->input('new_user')){{ 'checked' }}@endif>
-                    <div class="p-checkbox__indicator"></div>
-                </label>
-                <label class="p-checkbox p-checkboxUnit__item">既存
-                    <input value=1 name="not_new_user"
-                           type="checkbox" @if(request()->input('not_new_user')){{ 'checked' }}@endif>
-                    <div class="p-checkbox__indicator"></div>
-                </label>
-                <label class="p-checkbox p-checkboxUnit__item">稼働中
-                    <input value=1 name="is_working"
-                           type="checkbox" @if(request()->input('is_working')){{ 'checked' }}@endif>
-                    <div class="p-checkbox__indicator"></div>
-                </label>
-                <label class="p-checkbox p-checkboxUnit__item">待機中
-                    <input value=1 name="is_not_working"
-                           type="checkbox" @if(request()->input('is_not_working')){{ 'checked' }}@endif>
-                    <div class="p-checkbox__indicator"></div>
-                </label>
-            </div>
-            {{--checkbox--}}
-
-            {{--Level Skill--}}
-            <div class="p-formGroupUnit--2col u-ff-column js-parent">
-                <div class="p-formGroupUnit--2col__title">
-                    <p class="c-text">スキル</p>
-                    <p class="c-text u-pl-15">経験</p>
-                </div>
-                @foreach($response->getRelLevelSkill() as $searched_skill_id => $searched_level_id)
-                    <div class="p-formGroupUnit--2col__itemForSkill js-content js-remove">
-                        <label class="p-formGroupUnit--2col__label" for="">
-                            <select name="skill_ids[]" id="skill_id" class="c-input">
-                                <option value="">選択してください</option>
-                                @foreach($response->getSkills() as $skill)
-                                    <option
-                                        value="{{ $skill->id }}" {{ $searched_skill_id === $skill->id ? 'selected' : '' }}>{{ $skill->name }}</option>
-                                @endforeach
-                            </select>
-                        </label>
-
-                        <label class="p-formGroupUnit--2col__label--after" for="">
-                            <select name="level_ids[]" id="" class="c-input">
-                                <option value="">選択してください</option>
-                                @foreach($response->getLevels() as $level)
-                                    <option
-                                        value="{{ $level->id }}" {{ $searched_level_id === $level->id ? 'selected' : '' }}>{{ $level->level }}</option>
-                                @endforeach
-                            </select>
-                        </label>
-                    </div>
-                @endforeach
-                {{--    itemForSkill      --}}
-                <img class="c-icon--clickable u-m0a js-add" src="/images/icons/icon_add.png" alt="">
-            </div>
-            {{--Level Skill--}}
-
-            {{--operation_start_month--}}
-            <div class="p-register__row u-mt-20">
-                <span class="p-register__title c-text--bold">営業開始月</span>
-                <div class="p-register__itemWrap u-ff-row">
-                    <div class="p-register__item u-w-50-pc">
-                        <input type="date"
-                               class="c-input"
-                               name="operation_start_month"
-                               value="{{ old('operation_start_month') }}">
-                    </div>
-                </div>
-            </div>
-            {{--operation_start_month--}}
-            <button type="submit"
-                    class="c-button--secondary p-searchBox__button js-project_search u-db u-m0a u-w-15-pc u-mt-30 js-project_search">
-                検索
-            </button>
-            <div class="p-searchBox">
-                <button type="submit" name="status" class="c-button u-m0a u-mt-30 u-w-15" value="1">今月営業開始</button>
-                <button type="submit" name="status" class="c-button u-m0a u-mt-30 u-w-15" value="2">未営業</button>
-                <button type="submit" name="status" class="c-button u-m0a u-mt-30 u-w-15" value="3">新規稼働</button>
-                <button type="submit" name="status" class="c-button u-m0a u-mt-30 u-w-15" value="4">既存稼働</button>
-                <button type="submit" name="status" class="c-button u-m0a u-mt-30 u-w-15" value="5">今月面談</button>
-            </div>
-        </form>
-        {{--  検索ボタン（dashboard連動）  --}}
         <p class="c-text u-mt-20">ユーザー数：<span class="c-text--bold">{{ $response->getUserCounts() }}</span>件</p>
         @include('admin.pages.user.list._RegularUserTable', ['response' => $response])
         {{ $response->getUsers()->appends(request()->query())->links('components.paginator') }}
