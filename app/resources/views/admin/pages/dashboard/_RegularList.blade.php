421e6a175e6923493c7952e49de502887482428e
hiroki <nakamurahiroki@hiroki.local>
2021-05-18 10:53:25 +0900

    update: list

--- app/resources/views/admin/pages/dashboard/_RegularList.blade.php
+++ app/resources/views/admin/pages/dashboard/_RegularList.blade.php
@@ -5,7 +5,7 @@
   <li class="p-list__item"><a class="c-text p-list__itemInner" href="{{ route('application.list') }}">応募状況一覧</a></li>
   <li class="p-list__item"><a class="c-text p-list__itemInner" href="{{ route('assignment.list') }}">稼働状況一覧</a></li>
 </ul>
-<ul class="p-list u-dn-sp">
+<ul class="p-list u-dn-sp u-mt-20">
     <li class="c-text p-list__item--header">マスタ管理</li>
     <li class="p-list__item"><a class="c-text p-list__itemInner" href="{{ route('skill.list') }}">スキルマスタ</a></li>
     <li class="p-list__item"><a class="c-text p-list__itemInner" href="{{ route('level.list') }}">レベルマスタ</a></li>
