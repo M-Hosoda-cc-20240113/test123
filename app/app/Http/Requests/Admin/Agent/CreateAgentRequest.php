ee3a9bddebe538ad328ce8f8ec34a189bea1a87e
MiyawakiNoriyuki <n.miyawaki.carecon@gmail.com>
2021-05-20 15:00:11 +0900

    fix

--- app/app/Http/Requests/Admin/Agent/CreateAgentRequest.php
+++ app/app/Http/Requests/Admin/Agent/CreateAgentRequest.php
@@ -25,21 +25,26 @@ class CreateAgentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required','string','max:40','unique:agents'],
-            'tel'  => ['regex:/^[0-9]{2,4}[0-9]{2,4}[0-9]{3,4}$/','digits_between:8,11', new InAgentsByTel($this->input('tel')),'nullable'],
+            'name' => ['required', 'string', 'max:40', 'unique:agents'],
+            'tel' => [
+                'regex:/^[0-9]{2,4}[0-9]{2,4}[0-9]{3,4}$/',
+                'digits_between:8,11',
+                new InAgentsByTel($this->input('tel')),
+                'nullable'
+            ],
         ];
     }
 
     public function messages(): array
     {
         return [
-          'name.required'   => '会社名を入力してください。',
-          'name.string'     => '予期せぬ値が入力されました。',
-          'name.max40'      => '会社名が長すぎます。',
-          'name.unique'     => 'この会社名はすでに使われています。。',
-          'tel.regex'             => '正しい番号を入力してください。',
-          'tel.digits_between'    => '電話番号は8-11桁で入力してください。',
-          'tel.unique'            => 'この電話番号はすでに使われています。',
+            'name.required' => '会社名を入力してください。',
+            'name.string' => '予期せぬ値が入力されました。',
+            'name.max40' => '会社名が長すぎます。',
+            'name.unique' => 'この会社名はすでに使われています。。',
+            'tel.regex' => '正しい番号を入力してください。',
+            'tel.digits_between' => '電話番号は8-11桁で入力してください。',
+            'tel.unique' => 'この電話番号はすでに使われています。',
         ];
     }
 }
