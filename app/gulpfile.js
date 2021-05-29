ac653fbd0a2c1da93c69514afaa9e985eeaa2333
hiroki <nakamurahiroki@hiroki.local>
2021-05-29 17:45:15 +0900

    update: module

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -4,16 +4,8 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 // style.scssをタスクを作成する
-gulp.task('sass', ()=>{
-    return gulp.src('./resources/style_guide/src/styleGuide/assets/sass/style.scss')   // style.scssファイルを取得
-    .pipe(sass({outputStyle : 'expanded'}))   // Sassのコンパイルを実行
-    .pipe(gulp.dest('./public/css'));    // cssフォルダー以下に保存
+gulp.task('default', function () {
+    return gulp.src('resources/sass/style.scss')   // style.scssファイルを取得
+        .pipe(sass({outputStyle : 'expanded'}))   // Sassのコンパイルを実行
+        .pipe(gulp.dest('public/css'));    // cssフォルダー以下に保存
 });
-
-//監視タスク
-gulp.task('sass-watch',()=>{
-    gulp.watch('./resources/style_guide/src/styleGuide/assets/sass/**/*.scss', gulp.task('sass'));
-});
-
-//デフォルトタスク
-gulp.task('default',gulp.task('sass-watch'));
