2864738fca028e1a1a6bb6aed9f222f8fda7d034
Noriyuki Miyawaki <n.miyawaki.carecon@gmail.com>
2021-05-31 11:29:14 +0900

    fix:admin side menu and footer and mypage

--- app/resources/views/front/pages/mypage/_sidebar.blade.php
+++ app/resources/views/front/pages/mypage/_sidebar.blade.php
@@ -3,10 +3,10 @@
     <li class="c-text p-list__item--header">メニュー</li>
     <li class="p-list__item"><a class="c-text p-list__itemInner" href="{{ route('password.request') }}">パスワード変更</a></li>
     <li class="p-list__item"><a class="c-text p-list__itemInner" href="{{ route('email.request') }}">メールアドレス変更</a></li>
-    <li class="p-list__item"><a class="c-text p-list__itemInner" href="{{ route('contact.input') }}">お問い合わせ</a></li>
     @auth
     <li class="p-list__item"><a class="c-text p-list__itemInner" href="{{ route('front.user.edit') }}">ユーザー情報編集</a></li>
     @if(!Auth::getUser()->is_admin)
+      <li class="p-list__item"><a class="c-text p-list__itemInner" href="{{ route('contact.input') }}">お問い合わせ</a></li>
       <li class="p-list__item"><a class="c-text p-list__itemInner" href="{{ route('front.user.skill.form') }}">スキル編集</a></li>
       <li class="p-list__item"><a class="c-text p-list__itemInner" href="{{ route('front.user.delete.form') }}">退会</a></li>
     @endif
