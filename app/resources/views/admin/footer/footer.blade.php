f9713e888d76e77e96a00df31bbfa591f7e120f4
Noriyuki Miyawaki <n.miyawaki.carecon@gmail.com>
2021-06-01 16:45:59 +0900

    fix:footer

--- app/resources/views/admin/footer/footer.blade.php
+++ app/resources/views/admin/footer/footer.blade.php
@@ -1,9 +1,9 @@
 <div class="l-footer">
   <footer class="p-footer">
     <div class="p-footer__linkWrap">
-      <a href="{{ route('user.policy') }}" class="p-footer__textLink">Aegis利用規約</a>
+      <a href="{{ route('feature.policy') }}" class="p-footer__textLink">Aegis利用規約</a>
       <a href="https://care-con.co.jp/" class="p-footer__textLink">運営会社</a>
-      <a href="{{ route('user.privacy.policy') }}" class="p-footer__textLink">個人情報保護方針</a>
+      <a href="{{ route('feature.privacy.policy') }}" class="p-footer__textLink">個人情報保護方針</a>
     </div>
     <p class="p-footer__copy"><span>&copy;</span>2021 Carecon Inc</p>
   </footer>
