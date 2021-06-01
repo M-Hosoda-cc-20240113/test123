a1b23c4e288464234bb9d194c6c2c0f342b392af
Noriyuki Miyawaki <n.miyawaki.carecon@gmail.com>
2021-06-01 15:54:28 +0900

    feat:policy page

---
+++ app/app/Http/Controllers/Front/Feature/FeatureController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Front\Feature;
+
+use App\Http\Controllers\Controller;
+
+class FeatureController extends Controller
+{
+  public function showUserPolicy()
+  {
+    return view('front.pages.features._user_policy');
+  }
+
+  public function showPrivacyPolicy()
+  {
+    return view('front.pages.features._privacy_policy');
+  }
+}
---
+++ app/resources/views/front/pages/features/_privacy_policy.blade.php
@@ -0,0 +1,23 @@
+@extends('front.layout')
+
+@section('title', '個人情報保護方針|Aegis')
+
+@section('body')
+  @component('front.header.header', ['href' => route('front.index')])
+    @include('front.header._link_admin_top')
+    @include('front.header._link_mypage')
+    @include('front.header._link_login_logout')
+  @endcomponent
+  @include('front.pages._drawer_contents')
+  @include('front.pages._loader')
+
+  <div class="l-container">
+    <div class="l-main">
+      <div class="p-mainItem u-mt-20">
+        <h1 class="p-level2Heading c-text--center">個人情報保護方針</h1>
+      </div>
+    </div>
+  </div>
+
+  @include('front.footer.footer')
+@endsection
