2d19e027d8e0ed1b6c02ca87e167321dfedd9b51
Noriyuki Miyawaki <n.miyawaki.carecon@gmail.com>
2021-06-03 12:32:26 +0900

    admin drawer update

--- app/resources/views/admin/pages/_drawer_contents.blade.php
+++ app/resources/views/admin/pages/_drawer_contents.blade.php
@@ -11,5 +11,6 @@
     <li class="u-mt-10"><a href="{{ route('level.list') }}">レベルマスタ</a></li>
     <li class="u-mt-10"><a href="{{ route('station.list') }}">駅マスタ</a></li>
     <li class="u-mt-10"><a href="{{ route('agent.list') }}">案件元会社マスタ</a></li>
+    <li class="u-mt-10"><a href="https://form.run/teams#">お問い合わせ（form.run）</a></li>
   </ul>
 </nav>
