73ba37b11215a04a304071ef75a483f62e75ecf7
Noriyuki Miyawaki <n.miyawaki.carecon@gmail.com>
2021-06-07 16:59:01 +0900

    fix:validation

--- app/app/Http/Requests/Front/sendChangePasswordRequest.php
+++ app/app/Http/Requests/Front/sendChangePasswordRequest.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Requests\Front;
 
+use App\Models\User;
 use App\Rules\InUsersEmail;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class sendChangePasswordRequest extends FormRequest
 {
@@ -24,8 +26,20 @@ class sendChangePasswordRequest extends FormRequest
      */
     public function rules(): array
     {
+        $users = User::all();
+        $user_email = $users->pluck('email')->toArray();
         return [
-            'email' => ['required', 'email', new InUsersEmail($this->input('email'))],
+            'email' => ['required', 'email', Rule::in($user_email)],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'email.in' => '登録済みのメールアドレスを入力してください。'
         ];
     }
 }
