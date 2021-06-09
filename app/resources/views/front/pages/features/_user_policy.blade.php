5da7dab7f5fef48c042ce9b2df9ac615da3d62a7
Noriyuki Miyawaki <n.miyawaki.carecon@gmail.com>
2021-06-09 18:18:49 +0900

    fix:policy

--- app/resources/views/front/pages/features/_user_policy.blade.php
+++ app/resources/views/front/pages/features/_user_policy.blade.php
@@ -15,24 +15,6 @@
     <div class="l-main">
       <div class="p-mainItem u-mt-20">
         <h1 class="p-level2Heading c-text--center">利用規約</h1>
-        <p class="u-fs-24 u-mt-10">第１条</p>
-        <ul class="u-fs-16 u-mt-5">
-          <li>1.</li>
-          <li>2.</li>
-          <li>3.</li>
-        </ul>
-        <p class="u-fs-24 u-mt-10">第２条</p>
-        <ul class="u-fs-16 u-mt-5">
-          <li>1.</li>
-          <li>2.</li>
-          <li>3.</li>
-        </ul>
-        <p class="u-fs-24 u-mt-10">第３条</p>
-        <ul class="u-fs-16 u-mt-5">
-          <li>1.</li>
-          <li>2.</li>
-          <li>3.</li>
-        </ul>
       </div>
     </div>
   </div>
