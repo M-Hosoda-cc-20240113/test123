f6a738102083061561c26a1da3da96b234b5e63d
Noriyuki Miyawaki <n.miyawaki.carecon@gmail.com>
2021-06-15 09:39:23 +0900

    fix:seeder

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -20,13 +20,13 @@ class DatabaseSeeder extends Seeder
         $this->call(AgentTableSeeder::class);
         $this->call(AreaTableSeeder::class);
         $this->call(StationTableSeeder::class);
-        $this->call(ProjectSeeder::class);
-        $this->call(RelLevelSkillUserTableSeeder::class);
-        $this->call(ApplicationTableSeeder::class);
-        $this->call(RelPositionProjectTableSeeder::class);
-        $this->call(RelProjectSkillTableSeeder::class);
-        $this->call(AssignmentTableSeeder::class);
-        $this->call(StatusTableSeeder::class);
-        $this->call(NoteTableSeeder::class);
+//        $this->call(ProjectSeeder::class);
+//        $this->call(RelLevelSkillUserTableSeeder::class);
+//        $this->call(ApplicationTableSeeder::class);
+//        $this->call(RelPositionProjectTableSeeder::class);
+//        $this->call(RelProjectSkillTableSeeder::class);
+//        $this->call(AssignmentTableSeeder::class);
+//        $this->call(StatusTableSeeder::class);
+//        $this->call(NoteTableSeeder::class);
     }
 }
