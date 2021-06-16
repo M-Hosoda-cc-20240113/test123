6191616f9a1e3b851507e20ac3ed7c23944e1f32
Noriyuki Miyawaki <n.miyawaki.carecon@gmail.com>
2021-06-16 14:31:43 +0900

    fix:pull request

--- app/resources/js/front/ListMoreOpen.js
+++ app/resources/js/front/ListMoreOpen.js
@@ -1,4 +1,12 @@
-export class MoreOpen {
+/**
+ * listやcheckboxなどで複数の要素を表示する際に制限する
+ *
+ * 1,listやcheckboxなどの要素一つ一つに任意のクラス(contents)をつける
+ * 2,もっと見るボタンに任意のクラス(more_open)をつける
+ * 3,表示したい要素の数(display_count)
+ *
+ */
+export class ListMoreOpen {
   constructor(more_open, contents, display_count) {
     this.more_open_btn = more_open;
     this.contents = contents;
@@ -7,6 +15,9 @@ export class MoreOpen {
     this.setEvent();
   }
 
+  /**
+   * initialize
+   */
   init() {
     this.contents.forEach((content, index) => {
       this.contentsHide(content, index);
@@ -23,29 +34,48 @@ export class MoreOpen {
     });
   }
 
+  /**
+   * on click event
+   */
   onClick() {
-    if (!this.more_open_btn.classList.contains('is_open')) {
+    if (!this.isActive()) {
       this.contents.forEach((content) => {
         this.moreOpen(content);
       });
       this.more_open_btn.textContent = '閉じる';
-      this.more_open_btn.classList.add('is_open');
+      this.more_open_btn.classList.add('js-list-more-open_active');
     } else {
       this.contents.forEach((content, index) => {
         this.contentsHide(content, index);
       });
       this.more_open_btn.textContent = '....もっと見る';
-      this.more_open_btn.classList.remove('is_open');
+      this.more_open_btn.classList.remove('js-list-more-open_active');
     }
   }
 
+  /**
+   * show contents
+   * @param {HTMLElement} content
+   */
   moreOpen(content) {
     content.style.display = 'block';
   }
 
+  /**
+   * hide contents
+   * @param {number} index
+   * @param {HTMLElement} content
+   */
   contentsHide(content, index) {
     if (index > this.display_count) {
       content.style.display = 'none';
     }
+  }
+
+  /**
+   * @return boolean
+   */
+  isActive() {
+    return this.more_open_btn.classList.contains('js-list-more-open_active');
   }
 }
--- app/resources/js/front/pages/index.js
+++ app/resources/js/front/pages/index.js
@@ -1,9 +1,9 @@
 import {TabContentsLink} from '../TabContentsLink';
 import {FAQ} from '../FAQ';
-import {MoreOpen} from '../MoreOpen';
+import {ListMoreOpen} from '../ListMoreOpen';
 
 export function index() {
-  const content_parent = document.querySelectorAll('.js-open');
+  const content_parent = document.querySelectorAll('.js-list-more-open_parent');
   const display_count = 10;
   /**
    * コンテンツとタブのリンク
@@ -19,9 +19,9 @@ export function index() {
    * もっと見るボタン
    */
   content_parent.forEach((element) => {
-    const more_open = element.querySelector('.js-more_open');
-    const contents = element.querySelectorAll('.js-contents');
+    const more_open = element.querySelector('.js-list-more-open_btn');
+    const contents = element.querySelectorAll('.js-list-more-open_child');
 
-    new MoreOpen(more_open, contents, display_count);
+    new ListMoreOpen(more_open, contents, display_count);
   });
 }
