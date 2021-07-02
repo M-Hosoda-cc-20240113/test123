e0134e00e979a99e392fd5fcf46a37adc21c13c8
Noriyuki Miyawaki <n.miyawaki.carecon@gmail.com>
2021-07-02 10:24:40 +0900

    update birthday validation

--- app/app/Rules/CanRegisterBirthday.php
+++ app/app/Rules/CanRegisterBirthday.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Validation\Rule;
 class CanRegisterBirthday implements Rule
 {
     protected $birthday;
+
     /**
      * Create a new rule instance.
      *
@@ -22,23 +23,21 @@ class CanRegisterBirthday implements Rule
     /**
      * Determine if the validation rule passes.
      *
-     * @param  string  $attribute
-     * @param  mixed  $value
+     * @param string $attribute
+     * @param mixed $value
      * @return bool
      */
     public function passes($attribute, $value)
     {
         $now = CarbonImmutable::now();
-        $year = substr($this->birthday,0,4);
-        $month = substr($this->birthday,4,2);
-        $day = substr($this->birthday,6,2);
+        $year = substr($this->birthday, 0, 4);
+        $month = substr($this->birthday, 4, 2);
+        $day = substr($this->birthday, 6, 2);
         if ((int)$year > $now->year) {
             return false;
-        }
-        elseif ((int)$month > 12) {
+        } elseif ((int)$month > 12) {
             return false;
-        }elseif ((int)$day > 31)
-        {
+        } elseif ((int)$day > 31) {
             return false;
         }
         return true;
