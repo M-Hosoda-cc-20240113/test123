af79311dc508d9ff3e4310976285e64740a1039d
Noriyuki Miyawaki <n.miyawaki.carecon@gmail.com>
2021-07-06 09:31:40 +0900

    fix:delete projects

---
+++ app/resources/js/admin/AllCheck.js
@@ -0,0 +1,47 @@
+export class AllCheck {
+  /**
+   * 1. 全選択用チェックボックスにjs-check-all
+   * 2. 各チェックボックスにjs-checkbox-list
+   */
+  constructor() {
+    this.check_all = document.querySelector('.js-check-all');
+    this.check_list = document.querySelectorAll('.js-checkbox-list');
+
+    this.init();
+    this.setEvent();
+  }
+
+  /**
+   * initialize
+   */
+  init() {
+  }
+
+  /**
+   * set event
+   */
+  setEvent() {
+    this.check_all.addEventListener('change', e => {
+      this.changeAll(e);
+    });
+  }
+
+  /**
+   * @param {Event} e
+   */
+  changeAll(e) {
+    if (this.check_all.checked) {
+      for (let i in this.check_list) {
+        if (this.check_list.hasOwnProperty(i)) {
+          this.check_list[i].checked = true;
+        }
+      }
+    }else{
+      for (let i in this.check_list) {
+        if (this.check_list.hasOwnProperty(i)) {
+          this.check_list[i].checked = false;
+        }
+      }
+    }
+  }
+}
--- app/resources/js/admin/pages/project_list.js
+++ app/resources/js/admin/pages/project_list.js
@@ -1,7 +1,14 @@
-import {Table} from '../Table';
-
-const table = document.querySelector('table');
+import {Modal} from '../../front/Modal';
+import {TabContentsLink} from '../../front/TabContentsLink';
+import {AllCheck} from '../AllCheck';
+import {Alert} from '../Alert';
+import {config_message} from '../../config_message';
 
 export function project_list() {
-  new Table(table);
+  const alert_button = document.querySelector('.js-alert-button');
+  const delete_message = config_message.project_delete_alert;
+
+  new AllCheck();
+  new Alert(alert_button, delete_message);
+  new Modal();
 }
