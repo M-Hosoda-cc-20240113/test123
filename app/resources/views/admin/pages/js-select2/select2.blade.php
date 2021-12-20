e8b58cf1d903bb6cad56bd51b3f174094d1fdf73
wakidebase <n.miyawaki.carecon@gmail.com>
2021-12-20 15:16:23 +0900

    add: select2

---
+++ app/resources/views/admin/pages/js-select2/select2.blade.php
@@ -0,0 +1,15 @@
+<!-- Select2.css -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/css/select2.min.css">
+<!-- jquery & iScroll -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Select2本体 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/js/select2.min.js"></script>
+
+
+<script>
+  $(document).ready(function () {
+    $('.js-select2').select2({
+      language: 'ja',
+    });
+  });
+</script>
